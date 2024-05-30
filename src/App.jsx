4e37387844fad1c9a898cd2fdfd669d06eccc911
Patrick Marques <patrickmarques2009@hotmail.com>
2024-05-30 17:12:03 -0300

    Finalização do projeto com busca de lista de tarefas do https://jsonplaceholder.typicode.com/todos/ e renderização na tela

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { Header } from "./components/Header"
 import { Footer } from "./components/Footer"
 import { TodoList } from "./components/TodoList"
-import { todoList } from "./data"
+import { useState, useEffect } from "react"
 
 function App() {
+
+  const [todoList, setTodoList] = useState([])
+
+  useEffect( () => {
+
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(json => setTodoList(json))
+
+  }, [])
 
   return (
 
