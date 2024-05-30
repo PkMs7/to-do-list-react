8f1ed60ddd03149e59525c2ae0e043d9fd6591bf
Patrick Marques <patrickmarques2009@hotmail.com>
2024-05-30 16:54:17 -0300

    Renderização das tarefas personalizadas de acordo com seu status

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,5 +1,24 @@
+function NoTasks() {
+
+    return <h2>Nenuma task disponível</h2>
+
+}
+
+function RenderTasks({tasks = []}) {
+
+    return tasks.map( (task) => (
+        <div 
+            className={"alert " + (task.completed ? "alert-success" : "alert-warning")}
+            key={task.id}
+        >
+            {task.id} - {task.title}
+        </div>
+    ))
+
+}
+
 export function TodoList({data = []}) {
 
-    return data.length === 0 ? <h2>Nenhuma task</h2> : <div>Possuimos Tasks</div>
+    return data.length === 0 ? <NoTasks /> : <RenderTasks tasks={data} />
 
 }
